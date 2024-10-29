09edd6528632834ca2806fda558b7b2e2a63760e
ramon <pedroramon.r.lima@gmail.com>
2024-10-29 12:42:26 -0300

    feat: Add R2DBC configuration class for auditing
    - Created R2dbcConfig class to enable R2DBC auditing support in the application.
    - Annotated with @Configuration and @EnableR2dbcAuditing for automatic auditing capabilities.

---
+++ src/main/java/br/com/pedroramon/backend/config/R2dbcConfig.java
@@ -0,0 +1,9 @@
+package br.com.pedroramon.backend.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.r2dbc.config.EnableR2dbcAuditing;
+
+@Configuration
+@EnableR2dbcAuditing
+public class R2dbcConfig {
+}
