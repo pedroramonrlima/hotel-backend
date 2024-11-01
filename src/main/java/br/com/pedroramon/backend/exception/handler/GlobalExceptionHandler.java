14ceb2af4c0d60fb11b2f229a705415a1b643ba2
ramon <pedroramon.r.lima@gmail.com>
2024-11-01 11:12:56 -0300

    refactor: update GlobalExceptionHandler to support reactive programming

--- src/main/java/br/com/pedroramon/backend/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/br/com/pedroramon/backend/exception/handler/GlobalExceptionHandler.java
@@ -2,14 +2,15 @@ package br.com.pedroramon.backend.exception.handler;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-
 import br.com.pedroramon.backend.exception.ErrorResponse;
 import br.com.pedroramon.backend.exception.InvalidIdException;
 import br.com.pedroramon.backend.exception.ResourceNotFoundException;
-import jakarta.servlet.http.HttpServletRequest;
+import reactor.core.publisher.Mono;
+
 import java.util.HashMap;
 import java.util.Map;
 
@@ -29,58 +30,57 @@ public class GlobalExceptionHandler {
      * Manipula exceções do tipo {@link ResourceNotFoundException}.
      *
      * @param ex A exceção lançada.
-     * @param request O objeto {@code HttpServletRequest} que contém informações
+     * @param request O objeto {@code ServerHttpRequest} que contém informações
      *                sobre a requisição que causou a exceção.
-     * @return Uma {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
+     * @return Um {@link Mono} de {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
      *         com detalhes do erro e o status HTTP 404 (Not Found).
      */
     @ExceptionHandler(ResourceNotFoundException.class)
-    public ResponseEntity<ErrorResponse> handleResourceNotFoundException(
-            ResourceNotFoundException ex, jakarta.servlet.http.HttpServletRequest request) {
+    public Mono<ResponseEntity<ErrorResponse>> handleResourceNotFoundException(
+            ResourceNotFoundException ex, ServerHttpRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.NOT_FOUND.value(),
                 HttpStatus.NOT_FOUND.getReasonPhrase(),
                 ex.getMessage(),
-                request.getRequestURI()
+                request.getPath().value()
         );
-        return new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND);
+        return Mono.just(new ResponseEntity<>(errorResponse, HttpStatus.NOT_FOUND));
     }
 
     /**
      * Manipula exceções do tipo {@link InvalidIdException}.
      *
      * @param ex A exceção lançada.
-     * @param request O objeto {@code HttpServletRequest} que contém informações
+     * @param request O objeto {@code ServerHttpRequest} que contém informações
      *                sobre a requisição que causou a exceção.
-     * @return Uma {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
+     * @return Um {@link Mono} de {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
      *         com detalhes do erro e o status HTTP 400 (Bad Request).
      */
     @ExceptionHandler(InvalidIdException.class)
-    public ResponseEntity<ErrorResponse> handleInvalidIdException(
-        InvalidIdException ex, jakarta.servlet.http.HttpServletRequest request) {
+    public Mono<ResponseEntity<ErrorResponse>> handleInvalidIdException(
+            InvalidIdException ex, ServerHttpRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 ex.getMessage(),
-                request.getRequestURI()
+                request.getPath().value()
         );
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return Mono.just(new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST));
     }
 
     /**
      * Manipula exceções de validação de argumento do tipo {@link MethodArgumentNotValidException}.
      *
      * @param ex A exceção lançada.
-     * @param request O objeto {@code HttpServletRequest} que contém informações
+     * @param request O objeto {@code ServerHttpRequest} que contém informações
      *                sobre a requisição que causou a exceção.
-     * @return Uma {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
+     * @return Um {@link Mono} de {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
      *         com detalhes do erro e o status HTTP 400 (Bad Request), incluindo
      *         um mapa de campos inválidos e suas mensagens de erro.
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(
-        
-        MethodArgumentNotValidException ex, HttpServletRequest request) {
+    public Mono<ResponseEntity<ErrorResponse>> handleMethodArgumentNotValidException(
+            MethodArgumentNotValidException ex, ServerHttpRequest request) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(fieldError -> {
             errors.put(fieldError.getField(), fieldError.getDefaultMessage());
@@ -89,31 +89,30 @@ public class GlobalExceptionHandler {
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 "Erro de validação",
-                request.getRequestURI(),
+                request.getPath().value(),
                 errors
         );
-
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return Mono.just(new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST));
     }
 
     /**
      * Manipula exceções do tipo {@link IllegalArgumentException}.
      *
      * @param ex A exceção lançada.
-     * @param request O objeto {@code HttpServletRequest} que contém informações
+     * @param request O objeto {@code ServerHttpRequest} que contém informações
      *                sobre a requisição que causou a exceção.
-     * @return Uma {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
+     * @return Um {@link Mono} de {@link ResponseEntity} contendo um objeto {@link ErrorResponse}
      *         com detalhes do erro e o status HTTP 400 (Bad Request).
      */
     @ExceptionHandler(IllegalArgumentException.class)
-    public ResponseEntity<ErrorResponse> handleIllegalArgumentException(
-            IllegalArgumentException ex, jakarta.servlet.http.HttpServletRequest request) {
+    public Mono<ResponseEntity<ErrorResponse>> handleIllegalArgumentException(
+            IllegalArgumentException ex, ServerHttpRequest request) {
         ErrorResponse errorResponse = new ErrorResponse(
                 HttpStatus.BAD_REQUEST.value(),
                 HttpStatus.BAD_REQUEST.getReasonPhrase(),
                 ex.getMessage(),
-                request.getRequestURI()
+                request.getPath().value()
         );
-        return new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST);
+        return Mono.just(new ResponseEntity<>(errorResponse, HttpStatus.BAD_REQUEST));
     }
 }
